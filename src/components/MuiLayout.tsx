7faf27b6a1369a1533fcaf3d25e40624c98324c9
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-10-18 20:41:37 +0545

    stack

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,21 +1,42 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Divider } from "@mui/material";
 
 const MuiLayout = () => {
   return (
-    <Box
-      sx={{
-        backgroundColor: "primary.main",
-        color: "white",
-        height: "100px",
-        width: "100px",
-        padding: "16px",
-        "&:hover": {
-          backgroundColor: "primary.light",
-        },
-      }}
+    <Stack
+      sx={{ border: "1px solid" }}
+      direction="row"
+      spacing={2}
+      divider={<Divider orientation="vertical" flexItem />}
     >
-      MuiLayout
-    </Box>
+      <Box
+        sx={{
+          backgroundColor: "primary.main",
+          color: "white",
+          height: "100px",
+          width: "100px",
+          padding: "16px",
+          "&:hover": {
+            backgroundColor: "primary.light",
+          },
+        }}
+      >
+        MuiLayout
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "primary.main",
+          color: "white",
+          height: "100px",
+          width: "100px",
+          padding: "16px",
+          "&:hover": {
+            backgroundColor: "primary.light",
+          },
+        }}
+      >
+        MuiLayout
+      </Box>
+    </Stack>
   );
 };
 
