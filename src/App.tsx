a4fcfc3f34295e67c25eb5a3c78cd4e0f741745e
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-10-18 19:20:57 +0545

    checkbox, radiobox, rating icon and switch box

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import "./App.css";
 import MuiButton from "./components/MuiButton";
+import MuiCheckbox from "./components/MuiCheckbox";
+import MuiRadioButton from "./components/MuiRadioButton";
+import MuiRating from "./components/MuiRating";
 import MuiSelect from "./components/MuiSelect";
+import MuiSwitch from "./components/MuiSwitch";
 import MuiTextField from "./components/MuiTextField";
 import MuiTypography from "./components/MuiTypography";
 
@@ -10,7 +14,11 @@ function App() {
       {/* <MuiTypography /> */}
       {/* <MuiButton /> */}
       {/* <MuiTextField /> */}
-      <MuiSelect />
+      {/* <MuiSelect /> */}
+      {/* <MuiRadioButton /> */}
+      {/* <MuiCheckbox /> */}
+      {/* <MuiSwitch /> */}
+      <MuiRating />
     </div>
   );
 }
