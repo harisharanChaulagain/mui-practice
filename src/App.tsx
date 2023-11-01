3b3e2a812a1f223015f17b5707f333bfa272910d
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-11-01 17:53:04 +0545

    breadcrumbs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import MuiAccordion from "./components/MuiAccordion";
 import MuiAutocomplete from "./components/MuiAutocomplete";
+import MuiBreadcrumbs from "./components/MuiBreadcrumbs";
 import MuiButton from "./components/MuiButton";
 import MuiCard from "./components/MuiCard";
 import MuiCheckbox from "./components/MuiCheckbox";
@@ -32,7 +33,8 @@ function App() {
       {/* <MuiAccordion /> */}
       {/* <MuiImageList /> */}
       {/* <MuiNavbar /> */}
-      <MuiLinks />
+      {/* <MuiLinks /> */}
+      <MuiBreadcrumbs />
     </div>
   );
 }
---
+++ src/components/MuiBreadcrumbs.tsx
@@ -0,0 +1,27 @@
+import { Box, Breadcrumbs, Link, Typography } from "@mui/material";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+
+const MuiBreadcrumbs = () => {
+  return (
+    <Box m={2}>
+      <Breadcrumbs
+        aria-label="breacrumb"
+        separator={<NavigateNextIcon fontSize="small" />}
+        maxItems={2}
+      >
+        <Link underline="hover" href="#">
+          Home
+        </Link>
+        <Link underline="hover" href="#">
+          Catalog
+        </Link>
+        <Link underline="hover" href="#">
+          Accessories
+        </Link>
+        <Typography color="text.primary">Shoes</Typography>
+      </Breadcrumbs>
+    </Box>
+  );
+};
+
+export default MuiBreadcrumbs;
